3bf44fb7a7ba1556d0d9fc070ea4c6295b6110b3
Wallfacer-04 <2652531947@qq.com>
2024-12-14 15:50:27 +0800

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -110,8 +110,6 @@ void copyto(int x)
 		//其中包含Sleep(speed);
 	}
 	//放下物块 
-	isnowwood = false;
-	deletenowwood();
 	strcpy(emptyput[x], nowwood);
 
 	Printnum(emptywood.body[x].X + 2, emptywood.body[x].Y + 1, emptyput[x]);//格式化输出;
@@ -140,20 +138,67 @@ void copyfrom(int x)
 }
 void jump(int x, int* p)
 {
-	if (x > n)//错误情况
-	{
-		flag = 1;//标志出错
-		return;
-	}
 	*p = x - 1;//回到x上一个位置循环 
 }
 void jumpifzero(int x, int* p)
 {
-	if (x > n || isnowwood == false)//错误情况
+	if ( isnowwood == false)//错误情况
 	{
 		flag = 1;//标志出错
 		return;
 	}
 	if (nowwood[0] == '0')
 		*p = x - 1;
+}
+
+
+void inbox_4()
+{
+	while (nowx > 10)//贴到左边
+	{
+		movetoleft();
+		//其中包含Sleep(25);
+	}
+	if (inputnum == 0)//结束游戏
+	{
+		inboxnon = 1;
+		return;
+	}
+	//拿起木块
+	isnowwood = true;
+	strcpy(nowwood, input[0]);
+	PrintWood(nowx + 1, nowy - 3, input[0]);
+	//传送带动
+	for (int i = 0; i < inputnum; i++)
+	{
+		strcpy(input[i], input[i + 1]);
+		Printnum(inwood.body[i].X + 2, inwood.body[i].Y + 1, input[i]);
+	}
+	inputnum--;
+}
+
+void outbox_4()
+{
+	while (nowx < 33)
+	{
+		movetoright();
+		//其中包含Sleep(25)
+	}
+	if (isnowwood == false)//错误情况
+	{
+		system("cls");
+		flag = 1;//标志出错
+		return;
+	}
+	//放下木块
+	isnowwood = false;
+	outputnum++;
+	//传送带动
+	for (int i = outputnum; i > 0; i--)
+	{
+		strcpy(output[i], output[i - 1]);
+		Printnum(outwood.body[i].X + 2, outwood.body[i].Y + 1, output[i]);//格式化输出 
+	}
+	strcpy(output[0], nowwood);
+	Printnum(outwood.body[0].X + 2, outwood.body[0].Y + 1, output[0]);//格式化输出 
 }
